9ffd9720f92b2059607f8ac65e60089793ea4494
22008892 <22008892@10.175.4.207>
2023-07-11 14:39:24 +0800

    Test case for Retrieving Items

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -116,7 +116,7 @@ public class ResourceCentreTest {
 		//Test Case 1
 		assertNotNull("Test if there is valid Chromebook arraylist to add to", chromebookList);
 		assertEquals("Test that the Chromebook arraylist is empty.", 0, chromebookList.size());
-		// Attempt to retrieve the Camcoders 
+		// Attempt to retrieve the Chromebook 
 		String allChromebook= ResourceCentre.retrieveAllChromebook(chromebookList);
 		String testOutput = "";
 		// Test if the output is empty
@@ -127,7 +127,7 @@ public class ResourceCentreTest {
 		ResourceCentre.addChromebook(chromebookList, cb2);
 		// Test that the list is not empty
 		assertEquals("Test that Chromebook arraylist size is 2.", 2, chromebookList.size());
-		// Attempt to retrieve the Camcoders 
+		// Attempt to retrieve the Chromebook
 		allChromebook= ResourceCentre.retrieveAllChromebook(chromebookList);
 		testOutput = String.format("%-10s %-30s %-10s\n","CB0011", "My Google Chromebook 1st", "Mac OS");
 		testOutput += String.format("%-10s %-30s %-10s\n","CB0012", "SAMSUNG Chromebook 4+", "Win 10");
@@ -135,16 +135,16 @@ public class ResourceCentreTest {
 		assertEquals("Test that the display is correct.", testOutput, allChromebook);
 		
 		//Test Case 3
-		cc3.setIsAvailable(false);
+		cb3.setIsAvailable(false);
 		ResourceCentre.addChromebook(chromebookList, cb3);
-		assertEquals("Test that Camcorder arraylist size is 2.", 3, camcorderList.size());
+		assertEquals("Test that Chromebook arraylist size is 2.", 3, camcorderList.size());
 		assertFalse("Test that the last item in the arraylist is not available", camcorderList.get(2).getIsAvailable());
-		// Attempt to retrieve the Camcoders 
-		allCamcorder= ResourceCentre.retrieveAllCamcorder(camcorderList);
-		testOutput = String.format("%-10s %-30s %-10s %-10s %-20d\n","CC0011", "Nikon HDSLR", "Yes", "", 40);
-		testOutput += String.format("%-10s %-30s %-10s %-10s %-20d\n","CC0012", "Sony DSC-RX100M7", "Yes", "", 20);
+		// Attempt to retrieve the Chromebook
+		allChromebook= ResourceCentre.retrieveAllChromebook(chromebookList);
+		testOutput = String.format("%-10s %-30s %-10s\n","CB0011", "My Google Chromebook 1st", "Mac OS");
+		testOutput += String.format("%-10s %-30s %-10s\n","CB0012", "SAMSUNG Chromebook 4+", "Win 10");
 		// Test that the details are displayed correctly
-		assertEquals("Test that the display is correct.", testOutput, allCamcorder);
+		assertEquals("Test that the display is correct.", testOutput, allChromebook);
 		
 		
 	}
